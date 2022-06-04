5d66b60373ecceeadaf597dc18611eea8464e818
Junaid <junaidsaif8800@gmail.com>
2022-06-04 11:50:42 +0500

    font fixed

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - cy-b3lla',
-    title: 'cy-b3lla',
+    titleTemplate: '%s - CY-B3LLA',
+    title: 'CY-B3LLA',
     htmlAttrs: {
       lang: 'en',
     },
@@ -17,7 +17,11 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }, {
+      rel: "stylesheet",
+      href:
+        "https://fonts.googleapis.com/css2?family=Quantico:wght@400;700&display=swap",
+    },],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
